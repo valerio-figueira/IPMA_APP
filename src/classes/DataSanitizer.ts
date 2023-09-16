33ce56b7f3f3217fc0c008a9b7d33e2372710b87
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-16 09:51:50 -0300

    :recycle: update sanitizer class

--- src/classes/DataSanitizer.ts
+++ src/classes/DataSanitizer.ts
@@ -1,61 +1,23 @@
-import { IHolderRequest } from "../interfaces/IHolder";
-
-export default class DataSanitizer {
-  static convertToUpperCase(data: any): any {
-    if (!data) return null;
-    if (typeof data === 'string') {
-      return data.toUpperCase();
-    }
-  }
-
-  static convertEmptyToNull(data: any): any {
-    return data === '' ? null : data
-  }
-
-  static convertDateStringsToDate(data: any): any {
-    if (!data) return null;
-    if (typeof data === 'string') {
-      const date = new Date(data);
-      return isNaN(date.getTime()) ? null : date;
-    }
-  }
-
-  static sanitizeData(data: any): any {
-    if (data.cpf) data.cpf = data.cpf.replace(/\D/g, '');
-    if (data.identidade) data.identidade = data.identidade.replace(/\W/g, '');
-    if (data.celular_1) data.celular_1 = data.celular_1.replace(/\D/g, '');
-    if (data.celular_2) data.celular_2 = data.celular_2.replace(/\D/g, '');
-    if (data.tel_residencial) data.tel_residencial = data.tel_residencial.replace(/\D/g, '');
-  }
-
-  static convertData(data: IHolderRequest) {
-    this.sanitizeData(data);
-    for (const key in data) {
-      console.log(data[key])
-      if (data.hasOwnProperty(key)) {
-        data[key] = this.convertEmptyToNull(data[key]);
-        if(key !== 'email') {
-          data[key] = this.convertToUpperCase(data[key]);
-        }
+export default class StringSanitizer {
+  static convertToUpperCase(key: string, data: any) {
+      if (key === 'email') return data
+      if (!data) return null;
+      if (typeof data === 'string') {
+          return data.toUpperCase();
       }
-    }
+      return data
   }
 
-  static sanitizeObjectKeys(obj: any) {
-    for(let key in obj) {
-      if(key.match('contact.')) {
-        obj[key.replace('contact.', '')] = obj[key]
-        delete obj[key]
-      }
-      if(key.match('document.')) {
-        obj[key.replace('document.', '')] = obj[key]
-        delete obj[key]
-      }
-      if(key.match('location.')) {
-        obj[key.replace('location.', '')] = obj[key]
-        delete obj[key]
-      }
-    }
+  static sanitizeEmptyFields(data: any) {
+      return data === '' ? null : data
   }
 
+  static sanitizeLetters(text: string) {
+      if (typeof text === 'string') {
+          return text
+              .normalize('NFD')
+              .replace(/[\u0300-\u036f]/g, '');
+      }
+      return text
+  }
 }
