433c1a00a07c0d9c7bd4a9096d1ecf288c311922
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-16 09:52:11 -0300

    feat: add new package

---
+++ src/helpers/UserDataSanitizer.ts
@@ -0,0 +1,60 @@
+import StringSanitizer from "../classes/DataSanitizer"
+
+class UserDataSanitizer {
+    static sanitizeUserBody(data: any) {
+        for (const key in data) {
+            data[key] = this.sanitizeFields(key, data[key])
+            data[key] = StringSanitizer.convertToUpperCase(key, data[key])
+            data[key] = StringSanitizer.sanitizeEmptyFields(data[key])
+            data[key] = StringSanitizer.sanitizeLetters(data[key])
+        }
+        console.log(data)
+    }
+
+    static formatarCPF(cpf: any) {
+        return cpf.replace(/\D/g, '').replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    }
+
+    static sanitizeObjectKeys(obj: any) {
+        for (let key in obj) {
+            if (key.match('contact.')) {
+                obj[key.replace('contact.', '')] = obj[key]
+                delete obj[key]
+            }
+            if (key.match('document.')) {
+                obj[key.replace('document.', '')] = obj[key]
+                delete obj[key]
+            }
+            if (key.match('location.')) {
+                obj[key.replace('location.', '')] = obj[key]
+                delete obj[key]
+            }
+        }
+    }
+
+    static sanitizeFields(key: string, data: any) {
+        if (key.match('cpf')) return data.replace(/\D/g, '');
+        if (key.match('identidade')) return data.replace(/\W/g, '');
+        if (key.match('celular')) return data.replace(/\D/g, '');
+        if (key.match('residencial')) return data.replace(/\D/g, '');
+        return data
+    }
+
+    static sanitizeUserModel(model: any) {
+        const newData: any = {}
+        for (let key1 in model) {
+            for (let key2 in model[key1].dataValues) {
+                newData[key2] = model[key1][key2]
+            }
+        }
+        this.removeUnnecessaryID(newData)
+        newData.cpf = this.formatarCPF(newData.cpf)
+        return newData
+    }
+
+    static removeUnnecessaryID(obj: any) {
+        if (obj.id_documento) delete obj.id_documento
+        if (obj.id_contato) delete obj.id_contato
+        if (obj.id_localizacao) delete obj.id_localizacao
+    }
+}
