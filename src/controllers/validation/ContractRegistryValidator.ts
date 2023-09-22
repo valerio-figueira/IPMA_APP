6c088ebe0b4286e0bef0f5a887bfd9a324516f3d
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-22 17:01:38 -0300

    :sparkles: create validation

---
+++ src/controllers/validation/ContractRegistryValidator.ts
@@ -0,0 +1,20 @@
+import CustomError from "../../utils/CustomError";
+import IContractRegistry from "../../interfaces/IContractRegistry";
+
+export default class ContractRegistryValidator {
+
+    static validate(data: IContractRegistry) {
+        // Verifique se os campos obrigatórios estão presentes
+        if (!data.id_convenio && !data.id_titular) throw new CustomError('Verifique os campos obrigatórios', 400)
+
+        this.isNumber(data)
+    }
+
+    static isNumber(data: IContractRegistry) {
+        if(typeof data.id_convenio !== 'number') throw new CustomError('Verifique a identificação do convênio', 400)
+        if(data.id_dependente) {
+            if(typeof data.id_dependente !== 'number') throw new CustomError('Verifique a identificação do dependente', 400)
+        }
+        if(typeof data.id_titular !== 'number') throw new CustomError('Verifique a identificação do titular', 400)
+    }
+}
