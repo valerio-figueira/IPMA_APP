4721b53b1793e9302b394f3532679d74757de1ac
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-23 16:58:13 -0300

    :sparkles: create interface for billings

---
+++ src/interfaces/IBilling.ts
@@ -0,0 +1,11 @@
+export default interface IBilling {
+    id_mensalidade?: number;
+    id_titular: number;
+    valor: number;
+    mes_referencia: number;
+    ano_referencia: number;
+    status: 'Pendente' | 'Pago' | 'Anulado';
+    data_referencia: Date;
+    data_pagamento: Date | null;
+    data_registro?: Date;
+}
