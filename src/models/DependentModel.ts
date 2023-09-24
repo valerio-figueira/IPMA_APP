13d48f545686406b4d5d6a607d689d4763529e75
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-24 08:44:07 -0300

    :recycle: update dependent model

--- src/models/DependentModel.ts
+++ src/models/DependentModel.ts
@@ -2,10 +2,11 @@ import { Model, DataTypes } from 'sequelize';
 import Database from "../db/Database";
 import UserModel from './user/UserModel';
 import HolderModel from './HolderModel';
+import { IDependentBase } from '../interfaces/IDependent';
 
 const db = new Database;
 
-class DependentModel extends Model {
+class DependentModel extends Model<IDependentBase> {
   id_dependente!: number;
   id_usuario!: number;
   id_titular!: number;
@@ -47,5 +48,25 @@ DependentModel.init(
     timestamps: false,
   }
 );
+
+UserModel.hasOne(DependentModel, {
+  foreignKey: 'id_usuario',
+  as: 'dependent',
+});
+
+HolderModel.hasMany(DependentModel, {
+  foreignKey: 'id_titular',
+  as: 'dependent',
+});
+
+DependentModel.belongsTo(UserModel, {
+  foreignKey: 'id_usuario',
+  as: 'user',
+});
+
+DependentModel.belongsTo(HolderModel, {
+  foreignKey: 'id_titular',
+  as: 'holder',
+});
 
 export default DependentModel;
