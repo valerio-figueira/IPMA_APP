b062975b28d0b54cad69c3ebcee501a0f8f79031
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-30 16:50:11 -0300

    :sparkles: create user service

---
+++ src/services/UserService.ts
@@ -0,0 +1,36 @@
+import UserRepository from "../repositories/UserRepository";
+import CustomError from "../utils/CustomError";
+import { Contact, Document, Location, User, UserAttributes } from "../classes/UserSchema";
+
+export default class UserService {
+    userRepository: UserRepository;
+
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async Create(body: any) {
+        const user = new User(body)
+        const document = new Document(body)
+        const contact = new Contact(body)
+        const location = new Location(body)
+        const userData = new UserAttributes({ user, document, contact, location });
+
+        try {
+            return this.userRepository.Create(userData)
+        } catch (error: any) {
+            throw new CustomError('Não foi possível registrar o usuário', 400)
+        }
+    }
+
+    async ReadAll(query: any) { }
+
+    async ReadOne(user_id: string | number) {
+        return this.userRepository.ReadOne(user_id)
+    }
+
+    async Update() { }
+
+    async Delete(user_id: string | number) { }
+
+}
