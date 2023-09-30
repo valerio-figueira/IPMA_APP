f9dbcd749e6b6b15491048de896695904f27a88c
valerio-figueira <j.valerio.figueira@gmail.com>
2023-09-30 09:42:36 -0300

    :sparkles: add doctor model

---
+++ src/models/DoctorModel.ts
@@ -0,0 +1,64 @@
+import { Model, DataTypes } from 'sequelize';
+import Database from "../db/Database";
+import { IDoctor } from '../interfaces/IDoctor';
+
+const db = new Database;
+
+class DoctorModel extends Model<IDoctor> { }
+
+DoctorModel.init(
+    {
+        id_medico: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        codigo_prestador: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        nome_medico: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+        },
+        especialidade: {
+            type: DataTypes.STRING(60),
+            allowNull: false,
+        },
+        localidade: {
+            type: DataTypes.STRING(40),
+            allowNull: true,
+        },
+        cep: {
+            type: DataTypes.STRING(10),
+            allowNull: true,
+        },
+        endereco: {
+            type: DataTypes.STRING(60),
+            allowNull: true,
+        },
+        bairro: {
+            type: DataTypes.STRING(30),
+            allowNull: true,
+        },
+        telefone: {
+            type: DataTypes.STRING(40),
+            allowNull: true,
+        },
+        data_registro: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize: db.sequelize,
+        modelName: 'DoctorModel',
+        tableName: 'MEDICOS',
+        charset: 'utf8',
+        collate: 'utf8_unicode_ci',
+    }
+);
+
+
+export default DoctorModel
