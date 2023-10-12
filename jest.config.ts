913d4d5f7cbcc092175ce8072bffcd75f61f0373
valerio-figueira <j.valerio.figueira@gmail.com>
2023-10-12 18:08:52 -0300

    :sparkles: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+};
