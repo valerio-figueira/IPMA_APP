43e2a4ff19a37327f6efdfb43fd3f37c8902a150
valerio-figueira <j.valerio.figueira@gmail.com>
2023-10-12 18:06:57 -0300

    :recycle: create methods with transaction

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -6,6 +6,7 @@ import LocationModel from "../models/user/LocationModel";
 import { Transaction } from 'sequelize';
 import Database from "../db/Database";
 import Queries from "../db/Queries";
+import CustomError from "../utils/CustomError";
 
 type OptionalTransaction = Transaction | undefined;
 
@@ -16,17 +17,15 @@ export default class UserRepository {
         this.db = new Database();
     }
 
-    async Create(query: IUserAttributes, transaction: OptionalTransaction = undefined) {
-        if (!transaction) transaction = await this.db.sequelize.transaction()
-        const user = await UserModel.create(query.user, { transaction, raw: true });
+    async Create(query: IUserAttributes) {
+        const transaction = await this.db.sequelize.transaction()
 
-        this.insertIdValues(query, user.user_id);
-
-        const document = await DocumentModel.create(query.document, { transaction, raw: true });
-        const contact = await ContactModel.create(query.contact, { transaction, raw: true });
-        const location = await LocationModel.create(query.location, { transaction, raw: true });
-
-        return { user, document, contact, location }
+        try {
+            return this.CreateUserData(query, transaction)
+        } catch (error: any) {
+            transaction.rollback()
+            throw new CustomError('Falha ao registrar dados do usuário', 500)
+        }
     }
 
     async ReadAll() {
@@ -58,35 +57,74 @@ export default class UserRepository {
         })
     }
 
-    async Update(user_id: number, query: IUserAttributes, transaction: OptionalTransaction = undefined) {
-        if (!transaction) transaction = await this.db.sequelize.transaction()
+    async Update(user_id: number, query: IUserAttributes) {
+        const transaction = await this.db.sequelize.transaction()
 
+        try {
+            return this.UpdateUserData(user_id, query, transaction)
+        } catch (error: any) {
+            transaction.rollback()
+            throw new CustomError('Não foi possível atualizar os dados do usuário', 500)
+        }
+    }
+
+    async Delete(user_id: number | string) {
+        const transaction = await this.db.sequelize.transaction()
+
+        try {
+            await this.DeleteUserData(user_id, transaction)
+        } catch (error) {
+            transaction.rollback()
+            throw new CustomError('Não foi possível remover os dados do usuário', 500)
+        }
+    }
+
+    async CreateWithTransaction(query: IUserAttributes, transaction: Transaction) {
+        return this.CreateUserData(query, transaction)
+    }
+
+    async UpdateWithTransaction(user_id: number, query: IUserAttributes, transaction: Transaction) {
+        return this.UpdateUserData(user_id, query, transaction)
+    }
+
+    async DeleteWithTransaction(user_id: number | string, transaction: OptionalTransaction) {
+        await this.DeleteUserData(user_id, transaction)
+    }
+
+    private async CreateUserData(query: IUserAttributes, transaction: Transaction) {
+        const user = await UserModel.create(query.user, { transaction, raw: true });
+
+        this.insertIdValues(query, user.user_id);
+
+        const document = await DocumentModel.create(query.document, { transaction, raw: true });
+        const contact = await ContactModel.create(query.contact, { transaction, raw: true });
+        const location = await LocationModel.create(query.location, { transaction, raw: true });
+
+        return { user, document, contact, location }
+    }
+
+    private async UpdateUserData(user_id: number, query: IUserAttributes, transaction: Transaction) {
         const [user] = await UserModel.update(query.user, {
             where: { user_id },
             transaction
         })
 
         const [contact] = await ContactModel.update(query.contact, {
-            where: { user_id },
-            transaction
+            where: { user_id }, transaction
         })
 
         const [document] = await DocumentModel.update(query.document, {
-            where: { user_id },
-            transaction
+            where: { user_id }, transaction
         })
 
         const [location] = await LocationModel.update(query.location, {
-            where: { user_id },
-            transaction
+            where: { user_id }, transaction
         })
 
         return this.checkAffectedCount({ user, contact, document, location })
     }
 
-    async Delete(user_id: number | string, transaction: OptionalTransaction = undefined) {
-        if (!transaction) transaction = await this.db.sequelize.transaction()
-
+    private async DeleteUserData(user_id: number | string, transaction: OptionalTransaction) {
         await DocumentModel.destroy({
             where: { user_id }, transaction
         });
@@ -104,13 +142,13 @@ export default class UserRepository {
         });
     }
 
-    insertIdValues(data: IUserAttributes, user_id: number) {
+    private insertIdValues(data: IUserAttributes, user_id: number) {
         for (let key in data) {
             data[key].user_id = user_id
         }
     }
 
-    checkAffectedCount(data: any) {
+    private checkAffectedCount(data: any) {
         for (let entry in data) {
             if (data[entry]) return true
         }
