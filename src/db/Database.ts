464bd5c799953750970abdbf913ae3cdf35037ce
José Valério <j.valerio.figueira@gmail.com>
2023-10-18 13:09:55 -0300

    :recycle: update charset

--- src/db/Database.ts
+++ src/db/Database.ts
@@ -22,8 +22,8 @@ export default class Database {
             port: null || config.port,
             dialect: config.dialect,
             define: {
-                charset: 'utf8',
-                collate: 'utf8_general_ci',
+                charset: 'utf8mb4',
+                collate: 'utf8mb4_unicode_ci',
                 timestamps: true
             }
         })
