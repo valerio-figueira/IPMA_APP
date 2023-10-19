91e7bd787d1384cddb7786874fff2b7873eec564
José Valério <j.valerio.figueira@gmail.com>
2023-10-19 10:47:30 -0300

    :recycle: update tests

--- src/tests/services/AccessHierarchyService.test.ts
+++ src/tests/services/AccessHierarchyService.test.ts
@@ -1,12 +1,18 @@
 import 'jest'
 import AccessHierarchyService from '../../services/AccessHierarchyService'
 import AccessHierarchyModel from '../../models/AccessHierarchyModel'
+import CustomError from '../../utils/CustomError'
 import Database from '../../db/Database'
 
 
 const accessHierarchyService = new AccessHierarchyService()
 
 describe('TEST for Access Hierarchy Service', () => {
+    beforeAll(async () => {
+        const db = new Database()
+        await db.syncModels()
+        await db.clearDatabase()
+    })
 
     it('should create Root Access in Hierarchy', async () => {
         const newAccessHierarchy = {
@@ -18,6 +24,20 @@ describe('TEST for Access Hierarchy Service', () => {
 
         expect(response).toBeInstanceOf(AccessHierarchyModel)
         expect(response.level_name).toBe('root')
+    })
+
+    it('should not create Access if exists', async () => {
+        const newAccessHierarchy = {
+            level_name: 'root',
+            parent_level_id: null
+        }
+
+        try {
+            await accessHierarchyService.Create(newAccessHierarchy)
+        } catch (error: any) {
+            expect(error).toBeInstanceOf(CustomError)
+            expect(error.message).toBe('Já está registrado no sistema')
+        }
     })
 
     it('should create nested Adm Access in Hierarchy', async () => {
--- src/tests/services/SocialSecurityTeamService.test.ts
+++ src/tests/services/SocialSecurityTeamService.test.ts
@@ -1,26 +1,77 @@
 import 'jest'
 import SocialSecurityTeamService from '../../services/SocialSecurityTeamService'
-import { UserMock } from '../../utils/mocks/SSTMock'
+import { UserMock, SSTUserMock } from '../../utils/mocks/SSTMock'
+import AccessHierarchyService from '../../services/AccessHierarchyService'
+import Database from '../../db/Database'
+import CustomError from '../../utils/CustomError'
 
 
 const sstService = new SocialSecurityTeamService()
+const accessHierarchyService = new AccessHierarchyService()
+
+const resetDB = async () => {
+    const db = new Database()
+    await db.syncModels()
+    await db.clearDatabase()
+}
 
 describe('TEST for SST service', () => {
+    beforeAll(async () => {
+        await resetDB()
+        await accessHierarchyService.Create({
+            level_name: 'Root',
+            parent_level_id: null
+        })
+
+        await accessHierarchyService.Create({
+            level_name: 'Administrator',
+            parent_level_id: 1
+        })
+
+        await accessHierarchyService.Create({
+            level_name: 'Employee',
+            parent_level_id: 2
+        })
+    })
+
+
     it('should create new member for social security team', async () => {
-        
+
         const response = await sstService.Create(UserMock)
+        expect(response).toBeInstanceOf(Object)
+    })
 
-        console.log(response)
+    it('should insert another member in social security team', async () => {
 
+        const response = await sstService.Create(SSTUserMock)
         expect(response).toBeInstanceOf(Object)
     })
 
     it('should GET a member', async () => {
-        
-        const response = await sstService.ReadOne(1)
 
-        console.log(response)
+        const response = await sstService.ReadOne(2)
+        expect(response).toBeInstanceOf(Object)
+    })
+
+    it('should update a member in social security team', async () => {
+        SSTUserMock.name = 'Nikos Papadopoulos II'
+        SSTUserMock.sst_member_id = 1
+        const response = await sstService.Update(SSTUserMock)
 
         expect(response).toBeInstanceOf(Object)
+        expect(response).toHaveProperty('message')
+        expect(response?.message).toBe('Atualizado com sucesso')
+    })
+
+    it('should not update a member in social security team', async () => {
+        SSTUserMock.name = 'Nikos Papadopoulos II'
+        SSTUserMock.sst_member_id = 1
+
+        try {
+            await sstService.Update(SSTUserMock)
+        } catch (error: any) {
+            expect(error).toBeInstanceOf(CustomError)
+            expect(error.message).toBe('Não houve alterações')
+        }
     })
 })
