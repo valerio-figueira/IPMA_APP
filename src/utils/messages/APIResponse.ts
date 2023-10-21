783f272168f7ba0502c700c015bdbd7306764a30
valerio-figueira <j.valerio.figueira@gmail.com>
2023-10-21 14:01:28 -0300

    :recycle: add messages

--- src/utils/messages/APIResponse.ts
+++ src/utils/messages/APIResponse.ts
@@ -40,4 +40,49 @@ const APIResponse: IAPIResponse = {
     }
 }
 
+export const Success = {
+    STATUS: 200,
+    MESSAGE: 'Operação bem-sucedida'
+}
+
+export const Created = {
+    STATUS: 201,
+    MESSAGE: 'Recurso criado com sucesso'
+}
+
+export const BadRequest = {
+    STATUS: 400,
+    MESSAGE: 'Solicitação inválida'
+}
+
+export const NoContent = {
+    STATUS: 204,
+    MESSAGE: 'Nenhum conteúdo encontrado'
+}
+
+export const Unauthorized = {
+    STATUS: 401,
+    MESSAGE: 'Acesso não autorizado'
+}
+
+export const Forbidden = {
+    STATUS: 403,
+    MESSAGE: 'Acesso proibido'
+}
+
+export const NotFound = {
+    STATUS: 404,
+    MESSAGE: 'Não foi possível encontrar o recurso solicitado'
+}
+
+export const Conflict = {
+    STATUS: 409,
+    MESSAGE: 'Conflito na solicitação'
+}
+
+export const ServerError = {
+    STATUS: 500,
+    MESSAGE: 'Ocorreu um erro interno no servidor'
+}
+
 export default APIResponse
