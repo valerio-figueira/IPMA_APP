f27874766a91b9b2da390d09b4234e3b383fce77
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-02 18:49:45 -0300

    :sparkles: add fn to convert date

---
+++ src/helpers/ConvertDate.ts
@@ -0,0 +1,12 @@
+export function getJsDateFromExcel(excelDate: number) {
+    const SECONDS_IN_DAY = 24 * 60 * 60
+    const MISSING_LEAP_YEAR_DAY = SECONDS_IN_DAY * 1000
+    const MAGIC_NUMBER_OF_DAYS = (25567 + 2)
+
+    if (!Number(excelDate)) alert('wrong input format')
+
+    const delta = excelDate - MAGIC_NUMBER_OF_DAYS
+    const parsed = delta * MISSING_LEAP_YEAR_DAY
+    
+    return new Date(parsed)
+}
