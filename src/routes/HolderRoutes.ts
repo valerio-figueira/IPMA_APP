f2c7ba85866032e354a48db1f1e7f2a54003f7dc
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-02 18:51:26 -0300

    :recycle: new route for bulk create

--- src/routes/HolderRoutes.ts
+++ src/routes/HolderRoutes.ts
@@ -18,6 +18,7 @@ class HolderRoutes {
     initialize() {
         this.router.post('/', this.controller.Create.bind(this.controller))
         this.router.get('/', this.controller.ReadAll.bind(this.controller))
+        this.router.post('/bulk-create', this.controller.BulkCreate.bind(this.controller))
         this.router.get('/:id', this.controller.ReadOne.bind(this.controller))
         this.router.put('/', this.controller.Update.bind(this.controller))
         this.router.delete('/:id', this.controller.Delete.bind(this.controller))
