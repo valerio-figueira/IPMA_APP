58a7a0f65c543c0dc5f98977c1e7bf7a1aa55637
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-05 16:50:37 -0300

    :recycle: remove syncModel

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,6 @@ export default class Server {
         this.database = new Database()
         this.routes = new RegisterRoutes(this.APP, this.database)
         this.setupMiddleware()
-        this.database.syncModels()
         this.initializeRoutes()
     }
 
