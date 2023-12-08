58e723e2e0476c0be22ab5e09b35724c6ec0233f
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-08 13:07:00 -0300

    :recycle: new fn to validate and convert date

--- src/helpers/ConvertDate.ts
+++ src/helpers/ConvertDate.ts
@@ -1,3 +1,7 @@
+import { isValid, parse } from "date-fns"
+
+
+
 export function getJsDateFromExcel(excelDate: number) {
     const SECONDS_IN_DAY = 24 * 60 * 60
     const MISSING_LEAP_YEAR_DAY = SECONDS_IN_DAY * 1000
@@ -9,4 +13,14 @@ export function getJsDateFromExcel(excelDate: number) {
     const parsed = delta * MISSING_LEAP_YEAR_DAY
     console.log(parsed)
     return new Date(parsed)
+}
+
+
+
+export function validateAndConvertDate(value: string) {
+    // Tenta analisar a data
+    const date = parse(value, 'dd/MM/yyyy', new Date())
+
+    if (isValid(date)) return date
+    else return null
 }
