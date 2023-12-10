a93174e10d2ebbf2d58511ad54bca3644259b203
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-09 21:31:00 -0300

    :recycle: create backup fn

--- src/db/Database.ts
+++ src/db/Database.ts
@@ -1,8 +1,11 @@
 import { QueryTypes, Sequelize } from "sequelize";
 import DB_Config, { DB_ConfigType } from "./DatabaseConfig";
+import { DBErrors as ERROR } from "../utils/errors/Errors";
 import * as dotenv from "dotenv";
 import Models from "../models";
-import { DBErrors as ERROR } from "../utils/errors/Errors";
+import { exec } from "child_process";
+import * as path from "path";
+import { format } from "date-fns";
 dotenv.config();
 
 type envProps = undefined | 'test' | 'development' | 'production'
@@ -118,5 +121,58 @@ export default class Database {
     async dropIndex(tableName: string, indexName: string) {
         const sql = `DROP INDEX ${indexName} ON ${tableName}`;
         await this.sequelize.query(sql, { type: QueryTypes.RAW });
+    }
+
+
+
+
+    async backupDatabase() {
+        try {
+            if (this.environment !== 'development') {
+                throw new Error('Não é possível fazer backup em um ambiente que não seja de desenvolvimento.')
+            }
+
+            await this.authenticate()
+
+            const dumpCommand = this.createDumpCommand()
+
+            // Executa o comando mysqldump usando child_process
+            await new Promise((resolve, reject) => {
+                exec(dumpCommand, (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`Erro ao fazer backup do banco de dados: ${stderr}`)
+                        reject(error)
+                    } else {
+                        console.log(`Backup do banco de dados realizado com sucesso. Saída: ${stdout}`)
+                        resolve(true)
+                    }
+                })
+            })
+        } catch (error: any) {
+            throw new Error(`Erro ao fazer backup do banco de dados: ${error.message}`)
+        }
+    }
+
+
+
+
+    private createDumpCommand() {
+        const user = this.config.username
+        const host = this.config.host
+        const port = this.config.port
+        const dbname = this.config.database
+        const path = this.createFilePath()
+        return `mysqldump -u ${user} -p${this.config.password} -h ${host} --port ${port || ''} ${dbname} > ${path}`
+    }
+
+
+
+
+    private createFilePath() {
+        const currentTime = format(new Date(), 'dd-MM-yyyy')
+        const fileName = `backup-database-${currentTime}.sql`
+        const filePath = path.join(__dirname, '../backup', fileName)
+        console.log(filePath)
+        return filePath
     }
 }
