74fb5904d3229e3f04327a980248464c153e797e
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-09 21:31:18 -0300

    :recycle: add backup route

--- src/routes/DatabaseRoutes.ts
+++ src/routes/DatabaseRoutes.ts
@@ -19,6 +19,7 @@ class DatabaseRoutes {
         this.router.get('/authenticate', this.controller.Authenticate.bind(this.controller))
         this.router.get('/sync-models', this.controller.SyncModels.bind(this.controller))
         this.router.get('/clear-db', this.controller.ClearDatabase.bind(this.controller))
+        this.router.post('/backup', this.controller.Backup.bind(this.controller))
         this.router.post('/create-index', this.controller.CreateIndex.bind(this.controller))
         this.router.put('/update-index', this.controller.UpdateIndex.bind(this.controller))
         this.router.delete('/drop-index', this.controller.DropIndex.bind(this.controller))
