50027f9ece98cfc80f00577ddeca09916479f1ee
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-10 15:33:07 -0300

    :recycle: update logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -40,7 +40,7 @@ const prepareLoggerMessage = (req: Request, res: Response, options: any) => {
     if (options.ip) message.push(`Remote IP Address: ${options.ip}`);
     if (options.country) message.push(`Country: ${options.country}`);
     if (options.city) message.push(`City: ${options.city}`);
-    if (req.path === '/login') {
+    if (req.path.match('/login')) {
         const user = req.body.username
         message.push(res.statusCode === 200 ? `Login efetuado: ${user}` : `Tentativa de Login: ${user}`)
     }
@@ -56,27 +56,15 @@ export const loggerMessage = async (req: Request, res: Response) => {
     const decodedToken = JWT.getToken(req)
     const username = decodedToken ? decodedToken.user.username : null
 
-    // const response = await axios.get(`https://ipinfo.io/${remoteIpAddress}/json`)
-    // const data = response.data;
-
-    // Adicione informações de geolocalização ao objeto de solicitação (req)
-    /*const geolocation = {
-        ip: data.ip,
-        country: data.country,
-        region: data.region,
-        city: data.city,
-        loc: data.loc,  // latitude e longitude
-    }*/
-
     const message = prepareLoggerMessage(req, res, {
         ip: remoteIpAddress,
-        username
+        username: username
     })
 
     if (!message) return
-    if (req.path === '/login') return logger('info', 'login.log').info(message)
-    if (res.statusCode >= 200 && res.statusCode < 300) return logger('info', 'server.log').info(message);
-    if (res.statusCode >= 400) return logger('error', 'error.log').error(message);
+    if (req.path.match('/login')) return logger('info', 'login.log').info(message)
+    if (res.statusCode >= 200 && res.statusCode < 300) return logger('info', 'server.log').info(message)
+    if (res.statusCode >= 400) return logger('error', 'error.log').error(message)
 }
 
 export default logger
