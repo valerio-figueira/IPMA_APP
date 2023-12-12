f420af0e5620161d7d42f3c78c28f13ae72888e4
valerio-figueira <j.valerio.figueira@gmail.com>
2023-12-11 22:26:55 -0300

    :recycle: add appointments report

--- src/repositories/ReportRepository.ts
+++ src/repositories/ReportRepository.ts
@@ -16,8 +16,71 @@ export default class ReportRepository {
 
 
 
-    async BillingReport(query: any) {
-        const whereClause: any = {
+
+    async SumAppointments(query: Record<string, any>) {
+        const whereClause: Record<string, any> = {}
+
+        if (query.member_id) whereClause.member_id = query.member_id
+        if (query.reference_month) whereClause.reference_month = query.reference_month
+        if (query.reference_year) whereClause.reference_year = query.reference_year
+
+        return this.models.Appointment.findAll({
+            attributes: [
+                'member_id',
+                [this.db.sequelize.fn('SUM', this.db.sequelize.col('amount')), 'total_amount'],
+            ],
+            group: ['member_id'],
+        })
+    }
+
+
+
+
+    async AppointmentReport(query: Record<string, any>) {
+        const whereClause: Record<string, any> = {
+            '$subscription.active$': query.active || 1
+        }
+
+        if (query.holder_name)
+            whereClause['$subscription.holder.user.name$'] = query.holder_name
+
+        if (query.dependent_name)
+            whereClause['$subscription.dependent.user.name$'] = query.dependent_name
+
+        if (query.reference_year)
+            whereClause['reference_year'] = query.reference_year;
+
+        if (query.reference_month)
+            whereClause['reference_month'] = query.reference_month;
+
+        return this.models.Appointment.findAll({
+            where: whereClause, nest: true, raw: true,
+            include: [{
+                model: this.models.Member,
+                as: 'subscription',
+                include: [{
+                    model: this.models.Holder,
+                    as: 'holder',
+                    include: [{
+                        model: this.models.User,
+                        as: 'user'
+                    }]
+                }, {
+                    model: this.models.Agreement,
+                    as: 'agreement'
+                }]
+            }]
+        })
+    }
+
+
+
+
+
+
+
+    async BillingReport(query: Record<string, any>) {
+        const whereClause: Record<string, any> = {
             active: query.active || 1,
             '$billing.member_id$': { [Op.not]: null }
         }
