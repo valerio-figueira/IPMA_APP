dd063616b4974ecebf3a0c663a30b1ffb5030dd8
José Valério <j.valerio.figueira@gmail.com>
2024-01-12 20:32:53 -0300

    :recycle: add new class to convert sql data

---
+++ src/helpers/ConvertSQLData.ts
@@ -0,0 +1,30 @@
+
+
+
+class ConvertSQLData {
+    static convertDate(str: string) {
+        if (!str) return
+
+        const parts = str.split("-")
+        return `${parts[2]}/${parts[1]}/${parts[0]}`
+    }
+
+
+
+    static convertCPF(str: string) {
+        if (!str) return
+        return str.slice(0, 3) + '.' + str.slice(3, 6) + '.' + str.slice(6, 9) + '-' + str.slice(9, 11)
+    }
+
+
+
+    static convertPhoneNumber(str: string) {
+        if (!str) return
+        if (str.length === 11) return '(' + str.slice(0, 2) + ') ' + str.slice(2, 7) + '-' + str.slice(7, 11)
+        if (str.length === 10) return '(' + str.slice(0, 2) + ') ' + str.slice(2, 6) + '-' + str.slice(6, 11)
+    }
+}
+
+
+
+export default ConvertSQLData
