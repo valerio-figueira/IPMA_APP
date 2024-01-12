dd3023565023696a38934bfa6e480802c52f1927
José Valério <j.valerio.figueira@gmail.com>
2024-01-12 20:32:36 -0300

    :sparkles: add form repository

---
+++ src/repositories/FormRepository.ts
@@ -0,0 +1,33 @@
+import Database from "../db/Database";
+
+
+class FormRepository {
+    private db: Database
+
+
+    constructor(db: Database) {
+        this.db = db
+    }
+
+
+
+    async ReadHolderInfo(holder_id: string) {
+        return this.db.models.Holder.findOne({
+            where: { holder_id },
+            include: [{
+                model: this.db.models.User, as: 'user',
+                include: [{
+                    model: this.db.models.Document, as: 'document'
+                }, {
+                    model: this.db.models.Contact, as: 'contact'
+                }, {
+                    model: this.db.models.Location, as: 'location'
+                }]
+            }], raw: true, nest: true
+        })
+    }
+
+}
+
+
+export default FormRepository
