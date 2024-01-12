ee4fe96f9bd18b4e32b74f9039dc066c4ebbe018
José Valério <j.valerio.figueira@gmail.com>
2024-01-12 20:28:41 -0300

    :recycle: add new form fn and make clean code

--- src/services/FormService.ts
+++ src/services/FormService.ts
@@ -1,31 +1,63 @@
 import Database from "../db/Database";
 import UnimedForm from "../utils/UnimedForm";
+import UniodontoForm from "../utils/UniodontoForm";
+import FormRepository from "../repositories/FormRepository";
 import PDFDocument from 'pdfkit';
 import * as path from 'path';
 import * as fs from 'fs';
+import CustomError from "../utils/CustomError";
 
 
 class FormService {
-    private database: Database
+    private formRepository: FormRepository
+
 
     constructor(database: Database) {
-        this.database = database
+        this.formRepository = new FormRepository(database)
     }
+
+
+
+
+    createTempFile(fileName: string) {
+        const [month, year] = [new Date().getMonth() + 1, new Date().getFullYear()]
+        const filename = `${fileName}-${month}-${year}.pdf`
+        const filePath = path.join(__dirname, `../temp/${filename}`)
+        fs.createWriteStream(filePath)
+
+        return { filename, filePath }
+    }
+
+
+
 
 
     async CreateUnimedFormSubscription() {
         const doc = new PDFDocument({ size: 'A4' })
 
-        const [month, year] = [new Date().getMonth() + 1, new Date().getFullYear()]
-        const filename = `form-inscricao-unimed-${month}-${year}.pdf`
-        const filePath = path.join(__dirname, `../temp/${filename}`)
-        fs.createWriteStream(filePath)
+        const { filename, filePath } = this.createTempFile(`inscricao-unimed`)
 
         UnimedForm.drawForm(doc, [], {
             startX: 20,
             startY: 0,
             colWidths: []
         })
+
+        return { filename, filePath, doc }
+    }
+
+
+
+
+
+    async CreateUniodontoForm(holder_id: string) {
+        const holder = await this.formRepository.ReadHolderInfo(holder_id)
+        if (!holder) throw new CustomError('Usuário não encontrado!', 400)
+
+        const doc = new PDFDocument({ size: 'A4' })
+        const { filename, filePath } = this.createTempFile('inscricao-uniodonto')
+
+        UniodontoForm.drawForm(doc, { holderData: holder })
 
         return { filename, filePath, doc }
     }
