1bced3dda992b0d8f6f346626b6fe2f7f4cd60e0
José Valério <j.valerio.figueira@gmail.com>
2024-01-20 09:23:41 -0300

    :recycle: add new route

--- src/routes/MemberRoutes.ts
+++ src/routes/MemberRoutes.ts
@@ -19,6 +19,7 @@ class MemberRoutes {
         this.router.post('/', this.controller.Create.bind(this.controller))
         this.router.get('/', this.controller.ReadAll.bind(this.controller))
         this.router.post('/bulk-create', this.controller.BulkCreate.bind(this.controller))
+        this.router.post('/revive-member', this.controller.ReviveMember.bind(this.controller))
         this.router.get('/:id', this.controller.ReadOne.bind(this.controller))
         this.router.put('/', this.controller.Update.bind(this.controller))
         this.router.delete('/:id', this.controller.Delete.bind(this.controller))
