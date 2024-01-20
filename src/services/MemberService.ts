3c5ef58ba7770e4c444e75f4eafc06743cd773cf
José Valério <j.valerio.figueira@gmail.com>
2024-01-20 09:23:08 -0300

    :recycle: new fn and refactor deactive member fn

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,13 +1,9 @@
 import MemberRepository from "../repositories/MemberRepository";
 import IMember from "../interfaces/IMember";
 import CustomError from "../utils/CustomError";
-import HolderService from "./HolderService";
-import AgreementService from "./AgreementService";
 import MemberEntity from "../entities/MemberEntity";
 import MonthlyFeeService from "./MonthlyFeeService";
 import IMonthlyFee from "../interfaces/IMonthlyFee";
-import DependentService from "./DependentService";
-import HolderModel from "../models/HolderModel";
 import DependentModel from "../models/DependentModel";
 import MemberModel from "../models/MemberModel";
 import Database from "../db/Database";
@@ -21,17 +17,11 @@ import ExtractAndCreateData from "../helpers/ExtractAndCreateData";
 export default class MemberService {
     private db: Database;
     private memberRepository: MemberRepository;
-    private holderService: HolderService;
-    private agreementService: AgreementService;
     private monthlyFeeService: MonthlyFeeService;
-    private dependentService: DependentService;
 
     constructor(db: Database) {
         this.db = db
         this.memberRepository = new MemberRepository(db)
-        this.holderService = new HolderService(db)
-        this.dependentService = new DependentService(db)
-        this.agreementService = new AgreementService(db)
         this.monthlyFeeService = new MonthlyFeeService(db)
     }
 
@@ -39,10 +29,10 @@ export default class MemberService {
 
     @validateAgreements
     async Create(body: IMember & IMonthlyFee) {
-        const dependent = await this.findDependent(body)
-        await this.checkIfExists(body, dependent)
-        const holder = await this.findHolder(body)
-        const agreement = await this.findAgreement(body)
+        const dependent = await this.findDependent(body.dependent_id)
+        await this.checkIfMemberExists(body, dependent)
+        const holder = await this.findHolder(body.holder_id)
+        const agreement = await this.findAgreement(body.agreement_id)
 
         const transaction = await this.db.sequelize.transaction()
 
@@ -102,25 +92,19 @@ export default class MemberService {
     async Update(body: IMember) {
         const subscription = new MemberEntity(body)
 
-        if (!subscription.member_id) throw new CustomError('Verifique a identificação do conveniado', 400)
+        if (!subscription.member_id) throw new CustomError('Verifique a identificação do conveniado.', 400)
+        if (!subscription.holder_id) throw new CustomError('Verifique a identificação do servidor.', 400)
 
-        await this.checkIfMemberExists(body)
+        await this.checkMemberExistence(subscription.member_id)
 
-        if (!subscription.active && !subscription.exclusion_date) subscription.exclusion_date = new Date()
         if (subscription.active && subscription.exclusion_date) subscription.exclusion_date = null
 
         if (!subscription.active) {
-            if (subscription.holder_id && !subscription.dependent_id) {
-                const dependentExists = await this.findAllDependents(subscription)
-
-                if (dependentExists) {
-                    return this.memberRepository.updateExclusionOfDependents(body)
-                } else {
-                    const [affectedCount] = await this.memberRepository.Update(subscription)
-
-                    if (affectedCount) return this.ReadOne(subscription.member_id)
-                    else return affectedCount
-                }
+            if (!subscription.dependent_id) { // IT'S A HOLDER MEMBER BECAUSE DOESN'T HAVE DEPENDENT_ID
+                return this.deactiveMember(subscription, this.memberRepository
+                    .DeactiveMembers.bind(this.memberRepository))
+            } else { // IT'S DEPENDENT BECAUSE IT HAS DEPENDENT_ID
+                return this.deactiveMember(subscription, this.memberRepository.Update.bind(this.memberRepository))
             }
         } else {
             const [affectedCount] = await this.memberRepository.Update(subscription);
@@ -142,7 +126,7 @@ export default class MemberService {
             const agreement_id = holder.subscription.agreement_id
             const member_id = holder.subscription.member_id
             const holder_id = holder.holder_id
-            const agreement = await this.findAgreement({ agreement_id })
+            const agreement = await this.findAgreement(agreement_id)
             const deletedRegistry = await this.memberRepository.Delete({
                 holder_id, member_id, agreement_id
             })
@@ -179,13 +163,24 @@ export default class MemberService {
 
 
 
-    private async findDependent(body: IMember) {
-        if (!body.dependent_id) return
+    async ReviveMember(body: IMember) {
+        const memberEntity = new MemberEntity(body)
+        const dependent = await this.findDependent(memberEntity.dependent_id)
+        await this.checkIfMemberExists(memberEntity, dependent)
+        await this.findHolder(memberEntity.holder_id) // VALIDATE HOLDER
+        await this.findAgreement(memberEntity.agreement_id) // VALIDATE AGREEMENT
 
-        const dependent: DependentModel | null = await this.dependentService.ReadOne(body.holder_id, body.dependent_id)
+        return this.memberRepository.ReviveMember(memberEntity)
+    }
 
+
+
+
+
+    private async findDependent(dependent_id: string | number | null | undefined) {
+        if (!dependent_id) return
+        const dependent = this.db.models.Dependent.findByPk(dependent_id)
         if (!dependent) throw new CustomError('Não foi possível localizar os dados do dependente', 400)
-
         return dependent
     }
 
@@ -193,25 +188,24 @@ export default class MemberService {
 
 
 
-    private async findAllDependents(body: IMember) {
-        if (!body.dependent_id) return
+    private async deactiveMember(subscription: MemberEntity,
+        update: (e: MemberEntity) => Promise<[affectedCount: number]>) {
+        subscription.active = false
+        subscription.exclusion_date = new Date()
+        const [affectedCount] = await update(subscription)
 
-        const dependent: any[] = await this.dependentService.ReadAll(String(body.holder_id))
-
-        if (dependent.length === 0) throw new CustomError('Não foi possível localizar os dados do dependente', 400)
-
-        return dependent
+        if (affectedCount) return this.ReadOne(subscription.member_id!)
+        else return affectedCount
     }
 
 
 
 
 
-    private async findHolder(body: Record<string, any>) {
-        const holder: HolderModel = await this.holderService.ReadOne(body.holder_id)
 
-        if (!holder) throw new CustomError('Não foi possível localizar os dados do titular', 400)
-
+    private async findHolder(holder_id: string | number) {
+        const holder = await this.db.models.Holder.findByPk(holder_id)
+        if (!holder) throw new CustomError('Não foi possível localizar os dados do servidor', 400)
         return holder
     }
 
@@ -219,33 +213,39 @@ export default class MemberService {
 
 
 
-    private async findAgreement(body: Record<string, any>) {
-        const agreement = await this.agreementService.ReadOne(body.agreement_id);
-
+    private async findAgreement(agreement_id: string | number) {
+        const agreement = await this.db.models.Agreement.findByPk(agreement_id);
         if (!agreement) throw new CustomError('Não foi possível localizar os dados do convênio', 400)
-
         return agreement;
     }
 
 
 
 
-    private async checkIfExists(body: IMember, dependent: DependentModel | undefined) {
+
+
+    private async checkIfMemberExists(body: IMember, dependent: DependentModel | null | undefined) {
         if (dependent) {
             const dependentExists = await this.memberRepository.ifMemberExists(body, dependent.dependent_id)
-            if (dependentExists) throw new CustomError(`O dependente já existe na base de dados`, 400)
+            if (dependentExists) {
+                throw new CustomError(`O dependente já existe na base de dados`, 400)
+            }
             return
         }
 
         const member = await this.memberRepository.ifMemberExists(body)
-        if (member) throw new CustomError(`O titular já existe na base de dados`, 400)
+        if (member) { // IF MEMBER IS ENABLED
+            throw new CustomError(`O titular já existe na base de dados`, 400)
+        }
     }
 
 
 
 
-    private async checkIfMemberExists(body: IMember) {
-        const member = await this.memberRepository.ReadOne(body.member_id!)
+
+
+    private async checkMemberExistence(member_id: string | number) {
+        const member = await this.db.models.Member.findByPk(member_id)
 
         if (!member) throw new CustomError(`O conveniado não existe`, 400)
     }
@@ -289,31 +289,4 @@ export default class MemberService {
 
         return column
     }
-
-
-
-
-    private async addUsersToResponse(subscriptions: MemberModel[]) {
-        const holders: Record<number, any> = {}
-        let index = 1
-
-        for (let subscription of subscriptions) {
-            const holderID = subscription.holder_id
-            const dependentID = subscription.dependent_id
-
-            if (!holders[holderID]) {
-                const holder: any = await this.holderService.ReadOneSummary(holderID)
-                holder['subscriptions'] = {}
-                holders[holderID] = holder
-                index = 1
-            }
-            const dependent = dependentID ? await this.dependentService.ReadOneSummary(holderID, dependentID) : null
-            const agreementName = subscription['agreement']!['agreement_name']
-            holders[holderID]['subscriptions'][`${agreementName} ${index}`] = { ...subscription, dependent }
-            index++
-        }
-
-        return holders
-    }
-
 }
